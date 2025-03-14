0f0dedda1cf1d8443d2f8cfa48afb055d550db52
Luke Skinner <laskinner@gmail.com>
2025-03-14 15:24:22 +0800

    Changed page text

--- src/lib.rs
+++ src/lib.rs
@@ -19,7 +19,7 @@ pub fn App() -> impl IntoView {
         <Html attr:lang="en" attr:dir="ltr" attr:data-theme="light" />
 
         // sets the document title
-        <Title text="Welcome to Leptos CSR" />
+        <Title text="Welcome to the (very) unofficial UWA Accessibility Playground" />
 
         // injects metadata in the <head> of the page
         <Meta charset="UTF-8" />
